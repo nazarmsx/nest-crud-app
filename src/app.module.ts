2e11fad73c3fea9eb3275a9918be99d9e3c23ce6
Nazar <nazarmsx@ukr.net>
2025-05-17 18:06:56 +0300

    Prometheus monitoring added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,21 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { VersionMiddleware } from './middleware/version.middleware';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
   imports: [
     UserModule,
     ConfigModule.forRoot({ cache: false }),
     MongooseModule.forRoot(`${process.env.DATABASE_URI}/${process.env.DB_NAME}`),
+    PrometheusModule.register({
+      defaultLabels: {
+        app: 'Nest.js Crud App',
+      },
+      pushgateway: {
+        url: "http://127.0.0.1:9091",
+      },
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
