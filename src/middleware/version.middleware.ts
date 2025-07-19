b4f914269beb6087feb919f0ba0be039fa9ee826
Nazar <nazarmsx@ukr.net>
2025-07-20 00:13:40 +0300

    Dependencies updated.

--- src/middleware/version.middleware.ts
+++ src/middleware/version.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class VersionMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.setHeader('X-Api-Version', '1.0.1');
+    res.setHeader('X-Api-Version', '1.0.2');
     next();
   }
 }
